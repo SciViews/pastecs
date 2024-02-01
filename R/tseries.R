1516dc12d081e33079033f649a11508aa92f4361
Philippe Grosjean <phgrosjean@sciviews.org>
2024-02-01 14:05:17 +0100

    Elimination of the deprecated function is.R()

--- R/tseries.R
+++ R/tseries.R
@@ -1,29 +1,37 @@
-"tseries" <-
-function(x) {
-	if (is.null(class(x)) && class(x) != "regul" && class(x) != "tsd")
-		stop("x must be a 'regul' or a 'tsd' object")
-	if (class(x) == "regul") {
-		if (ncol(x$y) == 1) y <- x$y[[1]] else y <- as.matrix(x$y)
-		# We create a 'ts' object
-		res <- ts(y, start=x$tspar$start, frequency=x$tspar$frequency)
-		attr(res, "units") <- x$units
-	}
-	if (class(x) == "tsd") {
-		if (length(x$ts) == 1) {		# We have a decomposition of a single series
-			# x$series is already a ts or rts object
-			res <- x$series
-		} else {						# We have the decomposition of several series
-			# bind all series together
-			res <- x$series[[1]]
-			cnames <- paste(x$ts[1], ".", dimnames(x$series[[1]])[[2]], sep="")
-			for (i in 2:length(x$ts)) {
-				res2 <- x$series[[i]]
-				cnames <- c(cnames, paste(x$ts[i], ".", dimnames(x$series[[i]])[[2]], sep=""))
-				res <- cbind(res, res2)
-			}
-			dimnames(res)[[2]] <- cnames
-		}
-		attr(res, "units") <- x$units
-	}
-	res
+# TODO: make this a generic function with methods for tsd and regul objects
+tseries <- function(x) {
+  if (!inherits(x, c("regul", "tsd")))
+    stop("x must be a 'regul' or a 'tsd' object")
+
+  if (inherits(x, "regul")) {
+    if (ncol(x$y) == 1) {
+      y <- x$y[[1]]
+    } else {
+      y <- as.matrix(x$y)
+    }
+
+    # Create a 'ts' object
+    res <- ts(y, start = x$tspar$start, frequency = x$tspar$frequency)
+    attr(res, "units") <- x$units
+
+  } else if (inherits(x, "tsd")) {
+    if (length(x$ts) == 1) {# Decomposition of a single series
+      # x$series is already a ts or rts object
+      res <- x$series
+
+    } else {# De have the decomposition of several series
+      # Bind all series together
+      res <- x$series[[1]]
+      cnames <- paste(x$ts[1], ".", dimnames(x$series[[1]])[[2]], sep = "")
+      for (i in 2:length(x$ts)) {
+        res2 <- x$series[[i]]
+        cnames <- c(cnames, paste(x$ts[i], ".", dimnames(x$series[[i]])[[2]],
+          sep = ""))
+        res <- cbind(res, res2)
+      }
+      dimnames(res)[[2]] <- cnames
+    }
+    attr(res, "units") <- x$units
+  }
+  res
 }
--- R/turnogram.R
+++ R/turnogram.R
@@ -1,128 +1,187 @@
-"turnogram" <-
-function(series, intervals=c(1, length(series)/5), step=1, complete=FALSE, two.tailed=TRUE, FUN=mean, plotit=TRUE, level=0.05, lhorz=TRUE, lvert=FALSE, xlog=TRUE) {
-	call <- match.call()
-	data <- deparse(substitute(series))
-	fun <- deparse(substitute(FUN))
-	if (is.null(class(series)) || class(series) != "ts")
-		stop("series must be a single regular time series")
-	Unit <- attr(series, "units")
-	UnitTxt <- GetUnitText(series)
-	# Determine if there are many successive sequences of zeros
-	X <- as.vector(series)
-	N <- length(X)
-	strips <- c(X[1]-1, X[1:(N-1)]) != X | X != 0
-  	StrippedSeries <- X[strips]
-  	# Test the length of the series, range and step...
-  	n <- length(X)
-  	ns <- length(StrippedSeries)
-  	if (n > ns + 0.1 * n)
-  		warning("There are more than 10% of data as successive zeros, turnogram could be biased!")
-    if (length(intervals) < 2)
-    	stop("Interval must be a vector with 2 values: (min, max)")
-    if (intervals[1] < 1 || intervals[1] > n/3)
-    	stop("Interval must be larger or equal to 1, and smaller or equal to n/3")
-    if (intervals[2] < 1 || intervals[2] > n/3)
-    	stop("Interval must be larger or equal to 1, and smaller or equal to n/3")	
-    Inter.vec <- seq(intervals[1], intervals[2], step)
-    if (length(Inter.vec) < 2)
-    	stop("Less than 2 intervals. Redefine intervals or step")
-	n.vec <- nturns.vec <- nturns.min.vec <- nturns.max.vec <- Info.vec <- Info.min.vec <- Info.max.vec <- Inter.vec
-    
-    # Calculate the first step for the turnogram
-	turnogram.step1 <- function(Series, Interval, Complete, Fun) {
-		if (Complete == FALSE) {		# We just start intervals from the first observation
-			if (length(Series) %% Interval !=0) Series <- c(Series, rep(NA, Interval - (length(Series) %% Interval)))
-			dim(Series) <- c(Interval, length(Series) %/% Interval)
-			x <- apply(Series, 2, Fun, na.rm=TRUE)		# Because there is much chance to get some NAs appended at the end of the series!
-			n <- length(x)
-			Nturns <- turnpoints(x)$nturns
-			res <- list(n=n, nturns=Nturns)
-		} else {					# We calculate each possible interval
-			n <- Nturns.vec <- NULL
-			for (j in Interval:1) {
-				Ser <- Series[j:length(Series)]
-				if (length(Ser) %% Interval !=0) Ser <- c(Ser, rep(NA, Interval - (length(Ser) %% Interval)))
-				dim(Ser) <- c(Interval, length(Ser) %/% Interval)
-				x <- apply(Ser, 2, Fun, na.rm=TRUE)
-				Nturns.vec[j] <- turnpoints(x)$nturns	
-				n[j] <- length(x)
-			}
-			res <- list(n=n, nturns=Nturns.vec)
-		}
-		res
-	}
-    
-    # Calculate all first steps (n and nturns) for the turnogram
-	if (complete == FALSE) {
-		for (i in 1:length(Inter.vec)) {
-			res <- turnogram.step1(X, Inter.vec[i], Complete=complete, Fun=FUN)
-			n.vec[i] <- res$n
-			nturns.vec[i] <- res$nturns
-		}
-		# Calculate I (bits of information) according to either Gleissberg (n <= 50), or normal approximations (n > 50)
-		Info.vec <- -log(pgleissberg(n.vec, nturns.vec, two.tailed=two.tailed), base=2)
-		if (two.tailed == TRUE) {	# We have to change sign if k > mu.
-			rightpart <- nturns.vec > 2 * (n.vec - 2) / 3
-			Info.vec[rightpart] <- -Info.vec[rightpart]
-		}
-		# By default the extraction level is set to the interval corresponding to the maximum info value
-		Level <- Inter.vec[match(max(Info.vec), Info.vec)]
-		res <- list(interval=Inter.vec, n=n.vec, turns=nturns.vec, info=Info.vec, level=Level)
-	} else {
-		for (i in 1:length(Inter.vec)) {
-			res <- turnogram.step1(X, Inter.vec[i], Complete=complete, Fun=FUN)
-			n.vec[i] <- max(res$n)		# To change this!!!
-			nturns <- res$nturns
-			nturns.vec[i] <- mean(nturns)
-			nturns.min.vec[i] <- min(nturns)
-			nturns.max.vec[i] <- max(nturns)
-			infos <- -log(pgleissberg(res$n, nturns, two.tailed=two.tailed), base=2)
-			if (two.tailed == TRUE) {	# We have to change sign if k > mu.
-				rightpart <- nturns > 2 * (res$n - 2) / 3
-				infos[rightpart] <- -infos[rightpart]
-			}
-			Info.vec[i] <- mean(infos)
-			Info.min.vec[i] <- min(infos)
-			Info.max.vec[i] <- max(infos)
-			# By default the extraction level is set to the interval corresponding to the maximum info value
-			Level <- Inter.vec[match(max(Info.vec), Info.vec)]
-			res <- list(interval=Inter.vec, n=n.vec, turns=nturns.vec, turns.min=nturns.min.vec, turns.max=nturns.max.vec, info=Info.vec, info.min=Info.min.vec, info.max=Info.max.vec, level=Level)
-		}
-	}
-	as.data.frame(res)
-	res$call <- call
-	res$data <- data
-	if (complete == TRUE) res$type <- "Complete" else res$type <- "Simple"
-	res$fun <- fun
-	if (two.tailed == TRUE) res$proba <- "two-tailed probability" else res$proba <- "one-tailed probability"
-	res$units.text <- UnitTxt
-	attr(res, "units") <- Unit
-	
-	# Do we need to plot the graph for the turnogram?
-	if (plotit == TRUE) {
-		Ilevel <- -log(level, base=2)
-		if (xlog == TRUE) xlogstr <- "x" else xlogstr <- ""
-		if (two.tailed == TRUE) imin <- -1.1*Ilevel else imin <- 0
-		subtext <- paste(fun, "/", res$proba)
-		if (complete == FALSE) {
-			yrange.dat <- c(res$info, imin, 1.1*Ilevel)
-			yrange <- c(min(yrange.dat), max(yrange.dat))
-			plot(res$interval, res$info, type="l", log=xlogstr, ylim=yrange, xlab=paste("interval", UnitTxt), ylab="I (bits)", main=paste("Simple turnogram for:", data), sub=subtext)
-		} else {
-			yrange <- c(min(c(res$info.min, imin)), max(c(res$info.max, 1.1*Ilevel)))
-			plot(res$interval, res$info, type="l", log=xlogstr, ylim=yrange, xlab=paste("interval (", UnitTxt, ")", sep=""), ylab="I (bits)", main=paste("Complete turnogram for:", data), sub=subtext)
-			lines(res$interval, res$info.min)
-			lines(res$interval, res$info.max)
-		}
-		if (lhorz == TRUE) {
-			if (two.tailed == TRUE) {
-				abline(h=0)
-				abline(h=-Ilevel, lty=2, col=2)
-			}
-			abline(h=Ilevel, lty=2, col=2)
-		}
-		if (lvert == TRUE) abline(v=Level, lty=2, col=2)
-	}
-	class(res) <- "turnogram"
-	res 	# Return results
+# TODO: make a separate function to plot the turnogram
+turnogram <- function(series, intervals = c(1, length(series)/5), step = 1,
+    complete = FALSE, two.tailed = TRUE, FUN = mean, plotit = TRUE,
+    level = 0.05, lhorz = TRUE, lvert = FALSE, xlog = TRUE) {
+
+  call <- match.call()
+  data <- deparse(substitute(series))
+  fun <- deparse(substitute(FUN))
+  if (!inherits(series, "ts"))
+    stop("'series' must be a single regular time series")
+
+  units <- attr(series, "units")
+  units_txt <- GetUnitText(series)
+
+  # Determine if there are many successive sequences of zeros
+  x <- as.vector(series)
+  n <- length(x)
+  strips <- c(x[1] - 1, x[1:(n - 1)]) != x | x != 0
+  ns <- length(x[strips])
+  if (n > ns + 0.1 * n)
+    warning("There are more than 10% of data as successive zeros, turnogram could be biased!")
+
+  if (length(intervals) != 2)
+    stop("'intervals' must be a vector with 2 values: (min, max)")
+  if (any(intervals < 1) || any(intervals > n/3))
+    stop("'intervals' must be larger or equal to 1, and smaller or equal to n/3")
+
+  interv <- seq(intervals[1], intervals[2], step)
+  if (length(interv) < 2)
+    stop("Less than 2 intervals. Redefine intervals or step")
+
+  n_vec <- nturns_vec <- nturns_min_vec <- nturns_max_vec <-
+    info_vec <- info_min_vec <- info_max_vec <- interv
+
+  # Calculate the first step for the turnogram
+  turnogram_step1 <- function(series, interval, complete, fun) {
+    if (isFALSE(complete)) {# Just start intervals from the first observation
+
+      if (length(series) %% interval != 0)
+        series <- c(series, rep(NA, interval - (length(series) %% interval)))
+
+      dim(series) <- c(interval, length(series) %/% interval)
+      # There may be NAs appended at the end of the series
+      x <- apply(series, 2, fun, na.rm = TRUE)
+      n <- length(x)
+      nturns <- turnpoints(x)$nturns
+
+      res <- list(n = n, nturns = nturns)
+
+    } else {# Calculate each possible interval
+
+      n <- nturns_vec <- NULL
+
+      for (j in interval:1) {
+        ser <- series[j:length(series)]
+
+        if (length(ser) %% interval != 0)
+          ser <- c(ser, rep(NA, interval - (length(ser) %% interval)))
+
+        dim(ser) <- c(interval, length(ser) %/% interval)
+        # There may be NAs appended at the end of the series
+        x <- apply(ser, 2, fun, na.rm = TRUE)
+        nturns_vec[j] <- turnpoints(x)$nturns
+        n[j] <- length(x)
+      }
+      res <- list(n = n, nturns = nturns_vec)
+    }
+    res
+  }
+
+  # Calculate all first steps (n and nturns) for the turnogram
+  if (isFALSE(complete)) {
+    for (i in 1:length(interv)) {
+      res <- turnogram_step1(x, interv[i], complete = complete, fun = FUN)
+      n_vec[i] <- res$n
+      nturns_vec[i] <- res$nturns
+    }
+
+    # Calculate I (bits of information) according to either Gleissberg
+    # (n <= 50), or normal approximations (n > 50)
+    info_vec <- -log(pgleissberg(n_vec, nturns_vec, two.tailed = two.tailed),
+      base = 2)
+    if (isTRUE(two.tailed)) {# We have to change sign if k > mu.
+      rightpart <- nturns_vec > 2 * (n_vec - 2) / 3
+      info_vec[rightpart] <- -info_vec[rightpart]
+    }
+
+    # By default the extraction level is set to the interval corresponding to
+    # the maximum info value
+    level <- interv[match(max(info_vec), info_vec)]
+
+    res <- list(interval = interv, n = n_vec, turns = nturns_vec,
+      info = info_vec, level = level)
+  } else {
+
+    for (i in 1:length(interv)) {
+      res <- turnogram_step1(x, interv[i], complete = complete, fun = FUN)
+      n_vec[i] <- max(res$n) # To change this!!!
+      nturns <- res$nturns
+      nturns_vec[i] <- mean(nturns)
+      nturns_min_vec[i] <- min(nturns)
+      nturns_max_vec[i] <- max(nturns)
+      infos <- -log(pgleissberg(res$n, nturns, two.tailed = two.tailed),
+        base = 2)
+      if (isTRUE(two.tailed)) {# Change the sign if k > mu.
+        rightpart <- nturns > 2 * (res$n - 2) / 3
+        infos[rightpart] <- -infos[rightpart]
+      }
+      info_vec[i] <- mean(infos)
+      info_min_vec[i] <- min(infos)
+      info_max_vec[i] <- max(infos)
+
+      # By default the extraction level is set to the interval corresponding to
+      # the maximum info value
+      level <- interv[match(max(info_vec), info_vec)]
+
+      res <- list(interval = interv, n = n_vec, turns = nturns_vec,
+        turns.min = nturns_min_vec, turns.max = nturns_max_vec,
+        info = info_vec, info.min = info_min_vec, info.max = info_max_vec,
+        level = level)
+    }
+  }
+  as.data.frame(res)
+  res$call <- call
+  res$data <- data
+  if (isTRUE(complete)) {
+    res$type <- "Complete"
+  } else {
+    res$type <- "Simple"
+  }
+  res$fun <- fun
+  if (isTRUE(two.tailed)) {
+    res$proba <- "two-tailed probability"
+  } else {
+    res$proba <- "one-tailed probability"
+  }
+  res$units.text <- units_txt
+  attr(res, "units") <- units
+
+  # Plot the turnogram?
+  if (isTRUE(plotit)) {
+    info_level <- -log(level, base = 2)
+    if (isTRUE(xlog)) {
+      xlog_txt <- "x"
+    } else {
+      xlog_txt <- ""
+    }
+    if (isTRUE(two.tailed)) {
+      imin <- -1.1 * info_level
+    } else {
+      imin <- 0
+    }
+    sub_txt <- paste(fun, "/", res$proba)
+
+    if (isFALSE(complete)) {
+      yrange_dat <- c(res$info, imin, 1.1 * info_level)
+      yrange <- c(min(yrange_dat), max(yrange_dat))
+
+      plot(res$interval, res$info, type = "l", log = xlog_txt, ylim = yrange,
+        xlab = paste("interval", units_txt), ylab = "I (bits)",
+        main = paste("Simple turnogram for:", data), sub = sub_txt)
+    } else {
+
+      yrange <- c(min(c(res$info.min, imin)),
+        max(c(res$info.max, 1.1 * info_level)))
+
+      plot(res$interval, res$info, type = "l", log = xlog_txt, ylim = yrange,
+        xlab = paste0("interval (", units_txt, ")"), ylab = "I (bits)",
+        main = paste("Complete turnogram for:", data), sub = sub_txt)
+      lines(res$interval, res$info.min)
+      lines(res$interval, res$info.max)
+    }
+
+    if (isTRUE(lhorz)) {
+      if (isTRUE(two.tailed)) {
+        abline(h = 0)
+        abline(h = -info_level, lty = 2, col = 2)
+      }
+      abline(h = info_level, lty = 2, col = 2)
+    }
+    if (isTRUE(lvert))
+      abline(v = level, lty = 2, col = 2)
+  }
+
+  class(res) <- "turnogram"
+  res
 }
